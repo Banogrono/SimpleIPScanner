36d2bd702677b05f9f313a09c42fb445ff086486
v <012358132134@protonmail.ch>
2022-06-20 20:00:45 +0200

    Initial commit

---
+++ src/HostFinder.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3.8
+import os
+import socket
+import subprocess
+
+
+# tcp ports used by Linux, Windows and MacOS
+# ports = [20, 21, 22, 23, 25, 80, 111, 135, 137, 138, 139, 443, 445, 548, 631, 631, 993, 995]
+
+# ---------------------------------------------------------------------------------------------------------
+# Methods
+# ---------------------------------------------------------------------------------------------------------
+
+def find_hosts_by_scanning_ports(ip_lower_boundary=10, ip_upper_boundary=40, *args):
+    print("TCP start")
+
+    ports = args
+
+    # note to self: lists act like reference types in Java, ergo list in this function is passed by reference.
+    # (https://stackoverflow.com/questions/53459338/is-list-pass-by-value-or-by-reference)
+    addr_list = generate_ip_addresses_in_range(ip_lower_boundary, ip_upper_boundary)
+
+    online_list = []
+
+    for addr in addr_list:
+        for port in ports:
+            result = check_host(addr, port)
+            if result is not None:
+                online_list.append(result)
+
+    return online_list
+
+
+def print_out_found_hosts(online_list):
+    print(f"\nHosts found:")
+    for host in online_list:
+        print("Online:", host)
+
+
+def check_host(addr, port):
+    try:
+        test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        test_socket.settimeout(0.5)
+        result = test_socket.connect_ex((addr, port))
+        if result == 0:
+            test_socket.close()
+            # if host is found, return a tuple with IP address and open port
+            return addr
+    except socket.error:
+        print(socket.error)
+        return None
+    return None
+
+
+def generate_ip_addresses_in_range(ip_lower_boundary, ip_upper_boundary):
+    addresses = []
+    for i in range(ip_lower_boundary, ip_upper_boundary):
+        addresses.append('192.168.1.' + str(i))
+    return addresses
+
+
+def find_hosts_by_pinging(ip_lower_boundary=10, ip_upper_boundary=40):
+    print("ping start")
+    addresses = generate_ip_addresses_in_range(ip_lower_boundary, ip_upper_boundary)
+    active_hosts = []
+
+    for address in addresses:
+        # todo: there's gotta be a better way than calling subprocess
+        res = subprocess.call(["ping", address, "-c1", "-W2", "-q"], stdout=open(os.devnull, 'w'))
+        if res == 0:
+            active_hosts.append(address)
+
+    return active_hosts
+
+
+def get_device_name(list_of_hosts):
+    for host in list_of_hosts:
+        try:
+            result = dns.resolver.resolve(host, 'PTR')
+            print(result)
+        except Exception:
+            print("Something gone wrong for:", host)
---
+++ src/UIController.py
@@ -0,0 +1,80 @@
+import tkinter as tk
+from multiprocessing.pool import ThreadPool
+from tkinter import ttk
+import HostFinder as hF
+
+
+# ---------------------------------------------------------------------------------------------------------
+# methods
+# ---------------------------------------------------------------------------------------------------------
+
+def get_range_from_entry():
+    try:
+        result = parse_range(range_entry.get())
+        run_search(int(result[0]), int(result[1]))
+    except Exception as e:
+        print(e.with_traceback(None))
+
+
+def parse_range(string):
+    result = string.split("-")
+    return result
+
+
+def update_listbox(content):
+    for item in content:
+        listbox.insert(0, item)
+
+
+def run_search(address_lower, address_upper):
+    pool = ThreadPool(processes=1)
+    ping_async_result = pool.apply_async(hF.find_hosts_by_pinging, (address_lower, address_upper))
+    pool = ThreadPool(processes=2)
+    tcp_async_result = pool.apply_async(hF.find_hosts_by_scanning_ports, (address_lower, address_upper, 22, 445))
+
+    by_tcp = tcp_async_result.get()
+    by_ping = ping_async_result.get()
+
+    a = set(by_tcp)
+    b = set(by_ping)
+    c = a.union(b)
+
+    update_listbox(c)
+
+
+# ---------------------------------------------------------------------------------------------------------
+# program
+# ---------------------------------------------------------------------------------------------------------
+
+# root window
+root = tk.Tk()
+root.geometry("600x400")
+root.title('Simple IP Scanner')
+
+# configure the grid
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=3)
+root.columnconfigure(2, weight=1)
+
+# range label
+range_label = ttk.Label(root, text="Enter range:")
+range_label.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+
+# range entry
+range_entry = ttk.Entry(root)
+range_entry.grid(column=1, row=0, sticky="nsew", padx=5, pady=5)
+
+# Scan button
+scan_button = ttk.Button(root, text="Scan", command=get_range_from_entry)
+scan_button.grid(column=2, row=0, sticky=tk.E, padx=5, pady=5)
+
+# List with ip addresses
+listbox = tk.Listbox(
+    root,
+    height=16,
+    selectmode='browse',
+)
+
+listbox.grid(column=0, row=1, columnspan=3, sticky="nsew", padx=5, pady=5)
+
+root.mainloop()
